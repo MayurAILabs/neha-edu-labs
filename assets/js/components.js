f9bbd81b644117f3ba32a241df2bb8d51ecf5dc8
MayurAILabs <mayur.munshi1@gmail.com>
2026-07-10 00:01:39 +0530

    Update contact email to contact@nehaedulabs.online

--- assets/js/components.js
+++ assets/js/components.js
@@ -122,7 +122,7 @@
             '<li><a href="courses.html#electronics">Digital Electronics</a></li>' +
           '</ul></div>' +
           '<div><h4>Get in touch</h4><ul>' +
-            '<li><i class="fa-solid fa-envelope"></i>&nbsp; hello@nehaedulabs.in</li>' +
+            '<li><i class="fa-solid fa-envelope"></i>&nbsp; contact@nehaedulabs.online</li>' +
             '<li><i class="fa-solid fa-location-dot"></i>&nbsp; Pune, Maharashtra, India</li>' +
             '<li class="mt-2"><a href="contact.html" class="btn btn-outline btn-sm" style="border-color:rgba(255,255,255,.2);">Contact Us</a></li>' +
           '</ul></div>' +
